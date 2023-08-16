5245faf21369785109f057c87a734666ce30d253
thatboikham <khamjapher1@gmail.com>
2023-08-16 22:13:38 +0300

    Feat: Add rock paper sciccors to play 5 rounds
    the function allows the user to play the 5 rounds and later
    give the result who won or lost

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
 let promptAnswer = prompt("type rock paper or sciccors to start the game good luck!")
-let playerselection = promptAnswer.toLowerCase().toString
-
-const arr = ["rock", "paper", "sciccors"];
-const getrandomindex = Math.floor(Math.random() * 3);
-
-let computerselection = arr[getrandomindex]
+let playerselection = promptAnswer.toLowerCase()
 
 
-
-function playround(computerselection, playerselection){
-  if(playerselection === "sciccors" && computerselection === "rock" || playerselection === "rock" && computerselection === "paper" || playerselection === "paper" && computerselection === "sciccors"){
-    return `You lose! ${computerselection} beats ${playerselection}`
+function computerchoice(){
+  const arr = ["rock", "paper", "sciccors"];
+   return arr[Math.floor(Math.random() * 3)]
   }
-  else{
-    return `You WIN! ${playerselection} beats ${computerselection}`
+const computerselection = computerchoice();
+
+
+let winmessage = `You WIN! ${playerselection} beats ${computerselection}`
+let losemessage = `You lose! ${computerselection} beats ${playerselection}`
+let draw = `Its a draw`
+
+
+function game(){
+let playerscore = 0;
+let computerscore = 0;
+
+  for(let i = 0;i < 5; i++){
+   const computerselection = computerchoice();
+   console.log(`Round ${i + 1}`)
+   console.log(`computer Chose: ${computerselection}`)
+
+   function playround(computerselection, playerselection){
+    if(computerselection == playerselection){
+      return draw;
+    }
+    else if(playerselection =="sciccors" && computerselection == "rock" || playerselection == "rock" && computerselection == "paper" || playerselection == "paper" && computerselection =="sciccors"){
+      return `You lose! ${computerselection} beats ${playerselection}`;
   }
+    else{
+      return `You WIN! ${playerselection} beats ${computerselection}`;
+    }
   }
+
+   const result = playround(computerselection, playerselection)
+   console.log(result)
+   if(result == winmessage){
+    playerscore++;
+   }else if(result == losemessage){
+    computerscore++;
+   }
+
+}
+console.log(playerscore)
+console.log(computerscore)
+if(playerscore == computerscore){
+  console.log("its a tie")
+}
+else if(playerscore > computerscore){
+  console.log("You win all the overall play")
+}
+else{
+  console.log("You lost all rounds")
+}
+}
